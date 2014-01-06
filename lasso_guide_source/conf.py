651184d1ca96589782cb8e3895359108b6c979fb
Eric Knibbe <eric@lassosoft.com>
2014-01-06 19:18:02 +0000

    added OpenSearch functionality

--- lasso_guide_source/conf.py
+++ lasso_guide_source/conf.py
@@ -175,7 +175,7 @@ html_last_updated_fmt = '%b %d, %Y'
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+html_use_opensearch = 'http://lassoguide.com/'
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
